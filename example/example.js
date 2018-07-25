ec744af665d305548cc0d3367040eb35b0bcd37b
Anton <anton@adc.sh>
2018-07-25 07:32:07 +0100

    example

--- example/example.js
+++ example/example.js
@@ -1,6 +1,19 @@
 /* yarn example */
-import import from '../src'
+import { Replaceable } from 'restream'
+import ALaImport from '../src'
 
-(async () => {
-  await import()
+const STRING = `import aLaMode from 'alamode'
+import ALaImport from '@a-la/import'
+import App from 'koa'
+`
+
+;(async () => {
+  const stream = new Replaceable(
+    ALaImport,
+  )
+  await new Promise((r, j) => {
+    stream.end(STRING, r)
+    stream.on('error', j)
+  })
+  stream.pipe(process.stdout)
 })()
