9006cec9c0d041fa786e22a64245cb423815f6a1
Anton <anton@adc.sh>
2018-08-08 23:23:08 +0100

    fix named for correct whitespaces

--- src/lib/named.js
+++ src/lib/named.js
@@ -1,28 +1,27 @@
-import mismatch from 'mismatch'
 import { getRequire } from '.'
 
-const re = /^ *import\s*(?:([\w\d]+)\s*,)?\s*{\s*((?:,?\s*[\w\d]+(?:\s*as\s*[\w\d]+)?)*)\s*} from (["'])(.+?)\3/gm
+const re = /^ *import\s*(?:([\w\d]+)\s*,)?\s*{((?:\s*[\w\d]+(?:\s*as\s*[\w\d]+)?\s*,?\s*)*)} from (["'])(.+?)\3/gm
 
 /**
- * A rule to replace `import method from 'package'` statement.
+ * A rule to replace `import { method } from 'package'` statement.
  * @type {import('restream').Rule}
  */
 const rule = {
   re,
   replacement(match, def, methods, quotes, src) {
     const r = getRequire(quotes, src)
-    const m = mismatch(/([\w\d]+)(?:\s*as\s*([\w\d]+))?/g, methods, ['name', 'alias'])
-    const mm = m
-      .map(({ name, alias }) => (alias ? `${name}: ${alias}` : name) )
-      .join(', ')
+    const mm = methods.replace(
+      /([\w\d]+)(?:\s+as\s*([\w\d]+))?/g,
+      (_, name, alias) => {
+        return alias ? `${name}: ${alias}` : name
+      })
 
     if (def) {
       const s = `const ${def} = ${r}
-const { ${mm} } = ${def}
-`
+const {${mm}} = ${def}`
       return s
     } else {
-      const s = `const { ${mm} } = ${r}`
+      const s = `const {${mm}} = ${r}`
       return s
     }
   },
