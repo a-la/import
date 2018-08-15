3a73eca777a9b053952e872233ba2e5ff026746e
Anton <anton@adc.sh>
2018-08-15 18:30:51 +0100

    rule rename

--- test/spec/rule.js
+++ test/spec/rule.js
@@ -1,11 +1,11 @@
 import { resolve } from 'path'
 import { getTests } from 'zoroaster'
 import makeTestSuite from '../make-test-suite'
-import namedRule from '../../src/lib/named'
+import rule from '../../src/lib/rule'
 
 const path = resolve(__dirname, '../mask/named.js')
 const tests = getTests(path)
 
-const t = makeTestSuite(tests, namedRule)
+const t = makeTestSuite(tests, rule)
 
 export default t
