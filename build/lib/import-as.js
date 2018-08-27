b88159a056e920b0ee6cce720444e1a20673b90c
Anton <anton@adc.sh>
2018-08-27 07:01:52 +0300

    build semicolon, `def, * as`

--- build/lib/import-as.js
+++ build/lib/import-as.js
@@ -1,12 +1,24 @@
 const { replaceRequire } = require('.')
 
+const makeDefaultIf = (name) => {
+  const i = `if (${name} && ${name}.__esModule) ${name} = ${name}.default;`
+  return i
+}
+
 const importAs =
 {
-  re: / *import\s+\*\s+as\s+(.+?)(\s+from\s+(["'])(.+?)\3)/gm,
-  replacement(match, varName, fromSeg, quotes, src) {
+  re: /( *import\s+(?:(.+?)\s*,\s*)?\*\s+as\s+(.+?))(\s+from\s+(["'])(.+?)\5)/gm,
+  replacement(match, importSeg, defName, varName, fromSeg, quotes, src) {
     const r = replaceRequire(fromSeg, quotes, src)
-    const s = `const ${varName}${r}`
-    return s
+    const { length } = importSeg.split('\n')
+    const ws = '\n'.repeat(length - 1)
+    let c
+    if (defName) {
+      c = `${ws}let ${varName} = ${defName}${r} ${makeDefaultIf(defName)}`
+    } else {
+      c = `${ws}const ${varName}${r}`
+    }
+    return c
   },
 }
 
